23576ac232791d044de4a32176de3457d6e45c96
dyu <david.yu.ftw@gmail.com>
2022-01-08 15:51:53 +0800

    reorder

--- src/lib.zig
+++ src/lib.zig
@@ -4,35 +4,11 @@ const c = @cImport({
     @cInclude("zpp.h");
 });
 
-// --------------------------------------------------
-// c calling to zig
-
-export fn zpp_array_list_u8_append(
-    list_ptr: ?*anyopaque,
-    data: [*c]u8,
-    data_len: usize,
-) callconv(.C) bool {
-    if (list_ptr == null or data == null or data_len < 0) return false;
-    if (data_len == 0) return true;
-    
-    var list = @ptrCast(
-        *std.ArrayList(u8),
-        @alignCast(@alignOf(*std.ArrayList(u8)),
-        list_ptr,
-    ));
-    list.appendSlice(data[0..data_len]) catch return false;
-    
-    return true;
-}
-
-/// Access this variable so the compiler won't skip codegen for this
-pub const initialized = !zpp_array_list_u8_append(null, null, 0);
+const empty_array: [0]u8 = undefined;
+const empty_slice = empty_array[0..];
 
 // --------------------------------------------------
 // std::string
-
-const empty_array: [0]u8 = undefined;
-const empty_slice = empty_array[0..];
 
 pub const StdStringError = error {
     Append,
@@ -310,3 +286,27 @@ pub fn initFlexStdString(min_capacity: usize) FlexStdString {
         .buf = data[0..capacity],
     };
 }
+
+// --------------------------------------------------
+// c calling to zig
+
+export fn zpp_array_list_u8_append(
+    list_ptr: ?*anyopaque,
+    data: [*c]u8,
+    data_len: usize,
+) callconv(.C) bool {
+    if (list_ptr == null or data == null or data_len < 0) return false;
+    if (data_len == 0) return true;
+    
+    var list = @ptrCast(
+        *std.ArrayList(u8),
+        @alignCast(@alignOf(*std.ArrayList(u8)),
+        list_ptr,
+    ));
+    list.appendSlice(data[0..data_len]) catch return false;
+    
+    return true;
+}
+
+/// Access this variable so the compiler won't skip codegen for this
+pub const initialized = !zpp_array_list_u8_append(null, null, 0);
